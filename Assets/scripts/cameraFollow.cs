20db22488a1f436f004f590f7db31423e7962f3c
Danny Gorddy <70580929@continental.edu.pe>
2026-09-12 15:24:57 -0500

    Añadido Camera Follow para seguir al jugador

---
+++ Assets/scripts/cameraFollow.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    private void LateUpdate()
+    {
+        if (target != null)
+        {
+            transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        
+        }
+    }
+
+}
