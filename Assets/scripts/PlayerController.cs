00d47b85469488a97354187896747c1bda459211
Danny Gorddy <70580929@continental.edu.pe>
2026-09-12 14:24:05 -0500

    Añadida función de salto al personaje

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -5,13 +5,17 @@ public class PlayerController : MonoBehaviour
     public float speed =5;
     private Rigidbody2D rb2D;
     private float move;
-
-
-
+    public float jumpForce =4;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float groundRadius= 0.1f;
+    public LayerMask groundLayer;
+    private Animator animator;
 
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -19,8 +23,22 @@ public class PlayerController : MonoBehaviour
         move = Input.GetAxisRaw("Horizontal");
         rb2D.linearVelocity = new Vector2(move * speed, rb2D.linearVelocity.y);
         if(move != 0)
-        {
+        
             transform.localScale = new Vector3(Mathf.Sign(move), 1, 1);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+
+
+
+        {
+            rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, jumpForce);
         }
+        animator.SetFloat("Speed", Mathf.Abs(move));
+        animator.SetFloat("VerticalVelocity", rb2D.linearVelocity.y);
+
+    }
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
     }
 }
